499c373747a043b72588dacee31b6d45a589f2fc
erihsu <zhenyuxu163@163.com>
2022-04-06 22:39:40 +0800

    update CLI design

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ struct SvFmtArgs {
     /// format all files in the filelist
     #[clap(short = 'f')]
     pub filelist: Option<PathBuf>,
-    /// path to the files that need to be formatted
-    #[clap(short = 'p')]
-    pub path: Option<PathBuf>,
+    /// top file in the project
+    pub top: PathBuf,
     /// Additional including path
+    #[clap(long)]
     pub include: Vec<PathBuf>,
     /// excluded path, files under excluded path will not be formatted
     #[clap(long)]
@@ -38,7 +38,7 @@ impl Default for SvFmtArgs {
     fn default() -> Self {
         Self {
             filelist: None,
-            path: None,
+            top: PathBuf::new(),
             include: vec![],
             exclude: vec![],
             inplace: true,
@@ -54,7 +54,7 @@ impl Default for SvFmtArgs {
 fn main() -> std::io::Result<()>{
     // set termcolor
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+    
 
 
     let args = SvFmtArgs::parse();
@@ -78,39 +78,47 @@ fn main() -> std::io::Result<()>{
         let includes = filelist.incdirs;
         let paths = filelist.files;
         for path in paths {
-            if let Ok((syntax_tree, _)) = parse_sv(&path, &defines, &includes, false, false) {
-                writeln!(&mut stdout, "start to format {:?}",path)?;
-                stdout.reset()?;
-                let mut status = FormatStatus::new(&syntax_tree);
-                status.exec_format();
-                if args.debug {
-                    println!("[Output]");
-                    println!("{}",status.buffer);
-                }
-                // generate formatted code inplace or in outdir
-                if args.inplace {
-                    let mut opened_f = OpenOptions::new().write(true).truncate(true).open(path)?;
-                    opened_f.write_all(status.buffer.as_bytes())?;
-                    opened_f.flush()?;
+            if args.exclude.contains(&path) {
+                if let Ok((syntax_tree, _)) = parse_sv(&path, &defines, &includes, false, false) {
+                    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+                    writeln!(&mut stdout, "start to format {:?}",path)?;
+                    stdout.reset()?;
+                    let mut status = FormatStatus::new(&syntax_tree);
+                    status.exec_format();
+                    if args.debug {
+                        println!("[Output]");
+                        println!("{}",status.buffer);
+                    }
+                    // generate formatted code inplace or in outdir
+                    if args.inplace {
+                        let mut opened_f = OpenOptions::new().write(true).truncate(true).open(path)?;
+                        opened_f.write_all(status.buffer.as_bytes())?;
+                        opened_f.flush()?;
+                    } else {
+                        // FIXME
+                        svfmt_outdir.push(path.file_name().unwrap());
+                        let mut file = std::fs::File::create(svfmt_outdir.clone())?;
+                        file.write_all(status.buffer.as_bytes())?;
+                    }
                 } else {
-                    // FIXME
-                    svfmt_outdir.push(path.file_name().unwrap());
-                    let mut file = std::fs::File::create(svfmt_outdir.clone())?;
-                    file.write_all(status.buffer.as_bytes())?;
+                    println!("{:?} parse failed",path);
+                    return Ok(());
                 }
             } else {
-                println!("{:?} parse failed",path);
-                return Ok(());
+                stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+                writeln!(&mut stdout, "exclude {:?}, and will not be formatted",path)?;
+                stdout.reset()?;
             }
+
         }
     } 
-    // format from file
-    else if let Some(path) = args.path {
+    // format top file
+    else {
         let defines = HashMap::new();
-        let includes:Vec<PathBuf> = Vec::new();        
-        if let Ok((syntax_tree, _)) = parse_sv(&path, &defines, &includes, false, false) {
+        let includes:Vec<PathBuf> = args.include;        
+        if let Ok((syntax_tree, _)) = parse_sv(&args.top, &defines, &includes, false, false) {
             stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
-            writeln!(&mut stdout, "start to format {:?}",path)?;
+            writeln!(&mut stdout, "start to format {:?}",args.top)?;
             stdout.reset()?;
             let mut status = FormatStatus::new(&syntax_tree);
             status.exec_format();
@@ -120,21 +128,19 @@ fn main() -> std::io::Result<()>{
             }
             // generate formatted code inplace or in outdir
             if args.inplace {
-                let mut opened_f = OpenOptions::new().write(true).truncate(true).open(path)?;
+                let mut opened_f = OpenOptions::new().write(true).truncate(true).open(args.top)?;
                 opened_f.write_all(status.buffer.as_bytes())?;
                 opened_f.flush()?;
             } else {
                 // FIXME
-                svfmt_outdir.push(path.file_name().unwrap());
+                svfmt_outdir.push(args.top.file_name().unwrap());
                 let mut file = std::fs::File::create(svfmt_outdir.clone())?;
                 file.write_all(status.buffer.as_bytes())?;
             }
         } else {
-            println!("{:?} parse failed",path);
+            println!("{:?} parse failed",args.top);
             return Ok(());
         }
-    } else {
-        println!("You should specify file to format");
     }
 
     return Ok(());
